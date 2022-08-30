5f6ffb61ef4d7ea7b4455494c6047f4ea92577e8
Leonard Chi <leonardochi168@gmail.com>
2022-08-30 18:51:22 +0100

    Rotate Spaceship

--- space_rocks/models.py
+++ space_rocks/models.py
@@ -1,4 +1,9 @@
 from pygame.math import Vector2
+from pygame.transform import rotozoom
+
+from utils import load_sprite
+
+UP = Vector2(0, -1)
 
 class GameObject:
     def __init__(self, position, sprite, velocity):
@@ -8,8 +13,11 @@ class GameObject:
         self.velocity = Vector2(velocity)
 
     def draw(self, surface):
-        blit_position = self.position - Vector2(self.radius)
-        surface.blit(self.sprite, blit_position)
+        angle = self.direction.angle_to(UP)
+        rotated_surface = rotozoom(self.sprite, angle, 1.0)
+        rotated_surface_size = Vector2(rotated_surface.get_size())
+        blit_position = self.position - rotated_surface_size * 0.5
+        surface.blit(rotated_surface, blit_position)
 
     def move(self):
         self.position = self.position + self.velocity
@@ -17,3 +25,16 @@ class GameObject:
     def collides_with(self, other_obj):
         distance = self.position.distance_to(other_obj.position)
         return distance < self.radius + other_obj.radius
+
+class Spaceship(GameObject):
+    MANEUVERABILITY = 3
+    def __init__(self, position):
+        # Make a copy of the original UP vector
+        self.direction = Vector2(UP)
+
+        super().__init__(position, load_sprite("spaceship"), Vector2(0))
+
+    def rotate(self, clockwise = True):
+        sign = 1 if clockwise else -1
+        angle = self.MANEUVERABILITY * sign
+        self.direction.rotate_ip(angle)
