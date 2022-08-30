fc25c443c26b78c592490f668674ed887c5de449
Leonard Chi <leonardochi168@gmail.com>
2022-08-30 20:13:31 +0100

    Moving the Asteroids

--- space_rocks/utils.py
+++ space_rocks/utils.py
@@ -19,3 +19,8 @@ def wrap_position(position, surface):
 
 def get_random_position(surface):
     return Vector2(random.randrange(surface.get_width()), random.randrange(surface.get_height()))
+
+def get_random_velocity(min_speed, max_speed):
+    speed = random.randint(min_speed, max_speed)
+    angle = random.randrange(0, 360)
+    return Vector2(speed, 0).rotate(angle)
