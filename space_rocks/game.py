112add5eb83556f06297a463decfaeecabe27bf1
Leonard Chi <leonardochi168@gmail.com>
2022-08-30 20:18:47 +0100

    Colliding with the Spaceship

--- space_rocks/game.py
+++ space_rocks/game.py
@@ -41,16 +41,23 @@ class SpaceRocks:
 
         is_key_pressed = pygame.key.get_pressed()
 
-        if is_key_pressed[pygame.K_RIGHT] or is_key_pressed[pygame.K_d]:
-            self.spaceship.rotate(clockwise = True)
-        elif is_key_pressed[pygame.K_LEFT] or is_key_pressed[pygame.K_a]:
-            self.spaceship.rotate(clockwise = False)
-        if is_key_pressed[pygame.K_UP] or is_key_pressed[pygame.K_w]:
-            self.spaceship.accelerate()
+        if self.spaceship:
+            if is_key_pressed[pygame.K_RIGHT] or is_key_pressed[pygame.K_d]:
+                self.spaceship.rotate(clockwise = True)
+            elif is_key_pressed[pygame.K_LEFT] or is_key_pressed[pygame.K_a]:
+                self.spaceship.rotate(clockwise = False)
+            if is_key_pressed[pygame.K_UP] or is_key_pressed[pygame.K_w]:
+                self.spaceship.accelerate()
 
     def _process_game_logic(self):
         for game_object in self._get_game_objects():
             game_object.move(self.screen)
+
+        if self.spaceship:
+            for asteroid in self.asteroids:
+                if asteroid.collides_with(self.spaceship):
+                    self.spaceship = None
+                    break
 
     def _draw(self):
         self.screen.blit(self.background, (0, 0))
@@ -62,4 +69,9 @@ class SpaceRocks:
         self.clock.tick(60)
 
     def _get_game_objects(self):
-        return [*self.asteroids, self.spaceship]
+        game_objects = [*self.asteroids]
+
+        if self.spaceship:
+            game_objects.append(self.spaceship)
+
+        return game_objects
